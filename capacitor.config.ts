818449e972ab93b83bedfe066eeea5469b151f1d
João Victor Ribeiro Santos <joaovictoribeirosantos11@gmail.com>
2024-12-04 09:50:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'todo_app_mobile',
+  webDir: 'www'
+};
+
+export default config;
